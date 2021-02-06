fc389cf9b712b26240b9a2976086bf4e672870bf
genya.wakaki <wakawakawaka44@gmail.com>
2021-02-06 15:24:04 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 若木弦也 on 2021/02/06.
+//  Copyright © 2021 genya.wakaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
